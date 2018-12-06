d4e86a189e64e08ba14a0ca0f5a12e863f1b132d
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2018-12-06 19:29:55 +0100

    #! updated dependencies and resolve conflicts

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 let gulpConfig = require('./gulpConfig.js');
 
 module.exports = require('ima-gulp-task-loader')(
-  ['./node_modules/ima-gulp-tasks/tasks', './app/gulp/tasks'],
+  [
+    './node_modules/ima-gulp-tasks/tasks',
+    './node_modules/ima-examples/tasks',
+    './app/gulp/tasks'
+  ],
   gulpConfig
 );
