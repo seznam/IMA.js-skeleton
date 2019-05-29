034c9568a49972429fcb1416c0de2ee60d1383d6
Miroslav Jancarik <miroslav.jancarik@centrum.cz>
2019-05-29 11:25:39 +0200

    fixed for winston@3.x

--- server/server.js
+++ server/server.js
@@ -31,12 +31,12 @@ let helmet = require('helmet');
 let errorToJSON = require('error-to-json');
 let proxy = require('express-http-proxy');
 
-process.on('uncaughtException', error => {
-  logger.error('Uncaught Exception:', { error: errorToJSON(error) });
+process.on('uncaughtException', (error) => {
+	logger.error(`Uncaught Exception:\n${errorToJSON(error)}`);
 });
 
-process.on('unhandledRejection', error => {
-  logger.error('Unhandled promise rejection:', { error: errorToJSON(error) });
+process.on('unhandledRejection', (error) => {
+	logger.error(`Unhandled promise rejection:\n${errorToJSON(error)}`);
 });
 
 function renderApp(req, res, next) {
